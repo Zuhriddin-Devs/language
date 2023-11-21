835e5d6c3a40a65273aa8f57ad347dd3364023f9
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-11-21 17:35:24 +0500

    Add event name: DOMContentLoaded

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const elText = document.querySelector('#js-text');
-const elSelect = document.querySelector('#js-select');
+document.addEventListener('DOMContentLoaded', function () {
+    const elText = document.querySelector('#js-text');
+    const elLanguageSelect = document.querySelector('#js-select');
 
-elSelect.addEventListener('change', () => {
-    const selectLanguage = elSelect.value;
+    elLanguageSelect.addEventListener('change', function () {
+        let selectedLanguage = elLanguageSelect.value;
 
-    if (selectLanguage == 'uz') {
-        elText.textContent = "Salom!";
-    } else if (selectLanguage == 'en') {
-        elText.textContent = "Hello!";
-    } else if (selectLanguage == 'ru') {
-        elText.textContent = "Привет!";
-    };
+        if (selectedLanguage === 'uz') {
+            elText.textContent = 'Salom!';
+        } else if (selectedLanguage === 'en') {
+            elText.textContent = 'Hello!';
+        } else if (selectedLanguage === 'ru') {
+            elText.textContent = 'Привет!';
+        }
+    });
+
+    elText.textContent = 'Salom!';
 });
